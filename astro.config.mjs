7d37ecc198b1ce4c50471b213d6747d275f2d85b
MasterOogway713 <106326092+MasterOogway713@users.noreply.github.com>
2026-04-20 21:10:42 +0100

    Add sitemap integration and set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static'
+  // IMPORTANT: The sitemap needs to know your website's base URL
+  site: 'https://www.sbfloristry.co.uk', 
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
