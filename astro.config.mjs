2aad0ab0f15d9ee021abf3b8534ea020f5451a18
MasterOogway713 <106326092+MasterOogway713@users.noreply.github.com>
2026-05-19 21:04:26 +0100

    Add Cloudflare adapter to astro.config.mjs
    Added Cloudflare adapter for hybrid rendering support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sbfloristry.co.uk', 
+  
+  // Hybrid rendering compiles the site statically, but lets our GoCardless routes run SSR
+  output: 'hybrid',
+  adapter: cloudflare(),
+
   integrations: [
     tailwind(),
     sitemap()
   ],
   image: {
-    // This allows Astro to optimize images coming from your Shopify store
     domains: ['cdn.shopify.com'],
   },
-  // Enable prefetching for a faster "feel" when users hover over links
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover'
